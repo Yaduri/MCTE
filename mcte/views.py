0b1913b3db2d53de67bf9f992d3c00c2040c0a78
Yago Ribeiro <yaduri16@gmail.com>
2025-01-23 14:24:26 -0300

    Configs

--- mcte/views.py
+++ mcte/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+def render(request, nome_template:str, contexto = {}):
+    template = loader.get_template(nome_template)
+    return HttpResponse(template.render(contexto, request))
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'mcte/index.html')
 
 def login(request):
-    #return render(request, 'auth\login.html')
-    
     template = loader.get_template('auth/login.html')
     context = {}
     return HttpResponse(template.render(context, request))
