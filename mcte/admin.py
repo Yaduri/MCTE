59e4d3f18a636c26d7ab3a55abd86059ebb8ccf4
Yago Ribeiro <yaduri16@gmail.com>
2025-01-23 11:43:12 -0300

    Adicionando Tabelas

--- mcte/admin.py
+++ mcte/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Time, Treinador, Jogador, Carreira
+
+
+admin.site.register(Time)
+admin.site.register(Treinador)
+admin.site.register(Jogador)
+admin.site.register(Carreira)
--- mcte/models.py
+++ mcte/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 
 # Crie seus modelos aqui
 
+class Campeonato(models.Model):
+    nome = models.CharField(max_length=50)
+    #logo = Adicionar no futuro
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    
 class Time(models.Model):
     nome = models.CharField(max_length=50)
     #logo = Adicionar no futuro
@@ -23,3 +28,12 @@ class Jogador(models.Model):
     nome = models.CharField(max_length=50)
     data_nascimento = models.DateField('Data de Nascimento')
     carreira = models.ForeignKey(Carreira, on_delete=models.CASCADE)
+
+class Estatistica(models.Model):
+    jogos = models.IntegerField('Quantidade de Jogos')
+    gol = models.IntegerField('Quantidade de Gols')
+    assistencia = models.IntegerField('Quantidade de Assistencias')
+    jogador = models.ForeignKey(Jogador, on_delete=models.CASCADE)
+    campeonato = models.ForeignKey(Campeonato, on_delete=models.CASCADE)
+    carreira = models.ForeignKey(Carreira, on_delete=models.CASCADE)
+    
